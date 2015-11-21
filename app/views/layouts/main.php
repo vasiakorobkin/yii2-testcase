a141ec2ae6940fe87b4288c47ea50362cb95b253
Vasia Korobkin <vasyakorobkin@mail.ru>
2015-11-21 20:42:17 +0300

    Added top menu link for authorized users

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -33,20 +33,29 @@ AppAsset::register($this);
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
+
+    $navItems = [
+        ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'About', 'url' => ['/site/about']],
+        ['label' => 'Contact', 'url' => ['/site/contact']],
+    ];
+    if (Yii::$app->user->isGuest){
+        $navItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    } else {
+        array_unshift($navItems, [
+            'label' => 'Books',
+            'url' => ['book/index']
+        ]);
+        $navItems[] = [
+            'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+            'url' => ['/site/logout'],
+            'linkOptions' => ['data-method' => 'post']
+        ];
+    }
+
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ?
-                ['label' => 'Login', 'url' => ['/site/login']] :
-                [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ],
-        ],
+        'items' => $navItems,
     ]);
     NavBar::end();
     ?>
