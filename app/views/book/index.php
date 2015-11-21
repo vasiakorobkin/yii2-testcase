8e4862cba0d781e3ebeb4f5f1018021d11bcd801
Vasia Korobkin <vasyakorobkin@mail.ru>
2015-11-22 02:33:46 +0300

    book/index action view adjusted

--- app/views/book/index.php
+++ app/views/book/index.php
@@ -15,26 +15,66 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            [
+                'attribute' => 'id',
+                'label' => 'ID',
+            ],
+            [
+                'attribute' => 'name',
+                'label' => 'Название',
+            ],
+            [
+                'label' => 'Превью',
+                'format' => 'raw',
+                'value' => function($book)
+                {
+                    $url = $book['preview'];
+                    $imgTag = Html::img($url, ['alt' => $book['name'], 'class' => 'img-preview']);
+                    return Html::a($imgTag, $url, ['target' => '_blank']);
+                }
+
+            ],
+            [
+                'attribute' => 'author_fullname',
+                'label' => 'Автор',
+            ],
+            [
+                'attribute' => 'date',
+                'format' => 'date',
+                'label' => 'Дата выхода книги',
+            ],
+            [
+                'attribute' => 'date_create',
+                'format' => 'date',
+                'label' => 'Дата добавления',
+            ],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'header' => 'Кнопки действий',
+                'template' => '{update}<br>{view}<br>{delete}',
+                'buttons' => [
+                    'update' => function($url)
+                    {
+                        return Html::a('<span class="glyphicon glyphicon-pencil"></span> Ред', $url);
+                    },
+                    'view' => function($url)
+                    {
+                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span> Просм', $url);
+                    },
+                    'delete' => function($url)
+                    {
+                        return Html::a('<span class="glyphicon glyphicon-trash"></span> Удл', $url);
+                    },
+                ]
+            ]
+        ],
+    ]); ?>
+
     <p>
         <?= Html::a('Create Book', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'name',
-            'date_create',
-            'date_update',
-            'preview',
-            // 'date',
-            // 'author_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
 
 </div>
